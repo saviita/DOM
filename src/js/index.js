b8629eafd65e67b147014e595d0bb35b8f6b9471
saviita <taatt2000@gmail.com>
2024-06-20 11:09:55 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,7 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+const titleElement = document.getElementById('title');
+console.dir(titleElement);
+console.dir(titleElement.parentElement);
+console.dir(titleElement.nextElementSibling);
